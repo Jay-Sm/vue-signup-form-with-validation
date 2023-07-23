e20a0b741f6c16affe7aeba693acb93eb21c974e
Jay Sm <jaysmailg14@gmail.com>
2023-07-23 17:13:12 -0400

    rebuilt dist + minor vite config change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-signup-form-with-validation/',
+  base: '/vue-signup-form-with-validation',
   plugins: [vue()],
   resolve: {
     alias: {
